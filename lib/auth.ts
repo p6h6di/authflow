2b1d26e52ed32efb2e1ee7d9a922f0b4cf161268
Shivam Thakur <shivamth060@gmail.com>
2024-01-04 16:34:33 +0530

    Working with next-auth callbacks

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth"
+// import NextAuth from "next-auth"
 import authConfig from "@/auth.config"
 import {PrismaAdapter} from '@auth/prisma-adapter'
 import { prisma } from "@/prisma/client"
+import { UserRole } from "@prisma/client"
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +11,31 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  callbacks: {
+    async session({token, session}) {
+      if(token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if(token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({token}) {
+      // console.log({token})
+      if(!token.sub) return token;
+
+      const existingUser = await prisma.user.findUnique({
+        where: {
+          id: token.sub
+        }
+      })
+      if(!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    }
+  },
     adapter: PrismaAdapter(prisma),
     session: { strategy: 'jwt' },
     ...authConfig,
